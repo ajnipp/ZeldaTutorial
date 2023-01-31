d6aa10e95b3f0848f5406cbed9b628a5b9b1df96
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2023-01-31 17:56:23 -0500

    began tracking player movement

--- code/level.py
+++ code/level.py
@@ -1,5 +1,9 @@
 import pygame
 from settings import *
+from tile import Tile
+from player import Player
+from debug import debug
+
 class Level:
     def __init__(self):
 
@@ -8,17 +12,24 @@ class Level:
 
         # sprite group setup
         self.visible_sprites = pygame.sprite.Group()
-        self.obstacles_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
 
         # sprite setup
         self.create_map()
 
     def create_map(self):
         for row_index, row in enumerate(WORLD_MAP):
-            print(row_index)
-            print(row)
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+                if col == 'p':
+                    self.player = Player((x, y), [self.visible_sprites])
 
     def run(self):
 
         # update and draw the game
-        pass
+        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
