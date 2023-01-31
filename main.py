08cb364e81e1cfc707a68239877a1c6020bb6f2a
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2023-01-31 13:18:13 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame, sys
+from settings import *
+
+class Game:
+    def __init__(self):
+
+        # general setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
