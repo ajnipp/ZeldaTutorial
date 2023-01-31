251dd2eb279f2a70978ba0485e91e1ddac7c0338
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2023-01-31 16:33:41 -0500

    added assets, started map logic

--- code/main.py
+++ code/main.py
@@ -1,6 +1,6 @@
 import pygame, sys
 from settings import *
-
+from level import Level
 
 class Game:
     def __init__(self):
@@ -8,7 +8,10 @@ class Game:
         # general setup
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
+        pygame.display.set_caption("Zelda")
         self.clock = pygame.time.Clock()
+
+        self.level = Level()
 
     def run(self):
         while True:
@@ -18,6 +21,7 @@ class Game:
                     sys.exit()
 
             self.screen.fill('black')
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
 
