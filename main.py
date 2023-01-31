5df4e2d96e36e76201557c55335d6616727a3c12
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2023-01-31 16:03:38 -0500

    added starter game files

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import pygame, sys
 from settings import *
 
+
 class Game:
     def __init__(self):
 
         # general setup
         pygame.init()
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')
+            pygame.display.update()
+            self.clock.tick(FPS)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    game = Game()
+    game.run()
