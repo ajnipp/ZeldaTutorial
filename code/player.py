2a8f9d6fe1eaa840c03d468b09b8a62a0dc126d0
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2023-02-01 11:18:54 -0500

    documented Player class

--- code/player.py
+++ code/player.py
@@ -3,8 +3,9 @@ from settings import *
 from support import *
 from debug import *
 
+
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups,obstacle_sprites):
+    def __init__(self, pos, groups, obstacle_sprites):
         super().__init__(groups)
         self.image = pygame.image.load("graphics/test/player.png").convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
@@ -16,7 +17,6 @@ class Player(pygame.sprite.Sprite):
         self.frame_index = 0
         self.animation_speed = 0.15
 
-
         # movement
         self.direction = pygame.math.Vector2()
         self.speed = 5
@@ -27,19 +27,26 @@ class Player(pygame.sprite.Sprite):
         self.obstacle_sprites = obstacle_sprites
 
     def import_player_assets(self):
+        """
+        Populates self.animations dictionary with animations from the player graphics folder.
+        Uses the import_folder method to generate pygame surfaces for each frame and add them to
+        the appropriate list in the dictionary.
+        """
         character_path = 'graphics/player/'
         self.animations = {'up': [], 'down': [], 'left': [],
                            'right': [], 'right_idle': [], 'left_idle': [],
                            'down_idle': [], 'up_idle': [], 'right_attack': [],
-                           'left_attack': [],'up_attack': [], 'down_attack': []
-        }
+                           'left_attack': [], 'up_attack': [], 'down_attack': []
+                           }
 
         for animation in self.animations.keys():
             full_path = character_path + animation
             self.animations[animation] = import_folder(full_path)
 
     def get_status(self):
-
+        """
+        Updates the status of the player according to its previous status and current movement.
+        """
         # idle status
         if self.direction.x == 0 and self.direction.y == 0:
             if not 'idle' in self.status and not 'attack' in self.status:
@@ -56,7 +63,12 @@ class Player(pygame.sprite.Sprite):
         else:
             if 'attack' in self.status:
                 self.status = self.status.replace('_attack', '')
+
     def input(self):
+        """
+        Collect user input and update Player status and direction as needed. No updates to player state
+        will be made if the player is attacking.
+        """
         keys = pygame.key.get_pressed()
         if not self.attacking:
             # Vertical input
@@ -92,21 +104,32 @@ class Player(pygame.sprite.Sprite):
                 print('magic')
 
     def move(self, speed):
-        if self.direction.magnitude() != 0: # vector of 0 cannot be normalized
-            self.direction = self.direction.normalize() # prevents diagonal movement from being faster
+        """
+        Updates player position. Moves hitbox first, checks for collisions, then moves player to
+        position of hitbox.
+        :param speed: magnitude of velocity
+        """
+        if self.direction.magnitude() != 0:  # vector of 0 cannot be normalized
+            self.direction = self.direction.normalize()  # prevents diagonal movement from being faster
         self.hitbox.x += self.direction.x * speed
         self.collision('horizontal')
         self.hitbox.y += self.direction.y * speed
         self.collision('vertical')
-        self.rect.center = self.hitbox.center # rect follows the hitbox
+        self.rect.center = self.hitbox.center  # rect follows the hitbox
 
     def collision(self, direction):
+        """
+        Checks the hitbox of the player with the hitbox of all obstacle sprites. Uses the current player
+        direction to determine whether the collision is horizontal or vertical. Resets hitboxes
+        if there is a collision.
+        :param direction: the direction the current player movement. Either 'horizontal' or 'vertical'
+        """
         if direction == 'horizontal':
             for sprite in self.obstacle_sprites:
                 if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.x > 0: # moving right
+                    if self.direction.x > 0:  # moving right
                         self.hitbox.right = sprite.hitbox.left
-                    if self.direction.x < 0: # moving left
+                    if self.direction.x < 0:  # moving left
                         self.hitbox.left = sprite.hitbox.right
         if direction == 'vertical':
             for sprite in self.obstacle_sprites:
@@ -117,6 +140,10 @@ class Player(pygame.sprite.Sprite):
                         self.hitbox.bottom = sprite.hitbox.top
 
     def cooldowns(self):
+        """
+        Updates the player attacking property. If the attack_cooldown has elapsed since the time of the
+        first attack, then self.attacking is set to False.
+        """
         current_time = pygame.time.get_ticks()
 
         if self.attacking:
@@ -124,6 +151,9 @@ class Player(pygame.sprite.Sprite):
                 self.attacking = False
 
     def animate(self):
+        """
+        Sets the current image according to the current status and animation frame
+        """
         animation = self.animations[self.status]
 
         # loop over the frame index
@@ -133,6 +163,7 @@ class Player(pygame.sprite.Sprite):
         # set the image
         self.image = animation[int(self.frame_index)]
         self.rect = self.image.get_rect(center=self.hitbox.center)
+
     def update(self):
         self.input()
         self.cooldowns()
