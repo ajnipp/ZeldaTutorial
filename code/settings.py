ba9ca64cad710e1d8005660aa4377883a61c7a32
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2023-02-01 17:36:15 -0500

    ui for swapping weapons better

--- code/settings.py
+++ code/settings.py
@@ -6,11 +6,11 @@ TILESIZE = 64
 
 # weapons
 weapon_data = {
-    'sword': {'cooldown': 100, 'damage': 15, 'graphic': '/graphics/weapons/sword/full.png'},
-    'lance': {'cooldown': 400, 'damage': 30, 'graphic': '/graphics/weapons/lance/full.png'},
-    'axe': {'cooldown': 300, 'damage': 20, 'graphic': '/graphics/weapons/axe/full.png'},
-    'rapier': {'cooldown': 50, 'damage': 8, 'graphic': '/graphics/weapons/rapier/full.png'},
-    'sai': {'cooldown': 80, 'damage': 10, 'graphic': '/graphics/weapons/sai/full.png'}}
+    'sword': {'cooldown': 100, 'damage': 15, 'graphic': 'graphics/weapons/sword/full.png'},
+    'lance': {'cooldown': 400, 'damage': 30, 'graphic': 'graphics/weapons/lance/full.png'},
+    'axe': {'cooldown': 300, 'damage': 20, 'graphic': 'graphics/weapons/axe/full.png'},
+    'rapier': {'cooldown': 50, 'damage': 8, 'graphic': 'graphics/weapons/rapier/full.png'},
+    'sai': {'cooldown': 80, 'damage': 10, 'graphic': 'graphics/weapons/sai/full.png'}}
 
 # ui
 BAR_HEIGHT = 20
