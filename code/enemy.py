faa0f15162ed5c14a6ec58a3c6ef0f20c60b3bdb
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2023-02-03 10:40:33 -0500

    added enemy health reduction and knockback

--- code/enemy.py
+++ code/enemy.py
@@ -36,6 +36,12 @@ class Enemy(Entity):
         self.attack_time = None
         self.attack_cooldown = 400
 
+        # invincibility timer
+        self.vulnerable = True
+        self.hit_time = None
+        self.invincibility_duration = 300
+
+
     def import_graphics(self,name):
         self.animations = {'idle':[],'move':[],'attack':[]}
         main_path = f'graphics/monsters/{name}/'
@@ -72,10 +78,31 @@ class Enemy(Entity):
         else:
             self.direction = pygame.math.Vector2()
     def cooldown(self):
+        current_time = pygame.time.get_ticks()
         if not self.can_attack:
-            current_time = pygame.time.get_ticks()
             if current_time - self.attack_time >= self.attack_cooldown:
                 self.can_attack = True
+        if not self.vulnerable:
+            if current_time - self.hit_time >= self.invincibility_duration:
+                self.vulnerable = True
+
+    def get_damage(self,player,attack_type):
+        if self.vulnerable:
+            self.direction = self.get_player_distance_direction(player)[1]
+            if attack_type == 'weapon':
+                self.health -= player.get_full_weapon_damage()
+            else:
+                pass
+            self.hit_time = pygame.time.get_ticks()
+            self.vulnerable = False
+
+    def check_death(self):
+        if self.health < 0:
+            self.kill()
+
+    def hit_reaction(self):
+        if not self.vulnerable:
+            self.direction *= -self.resistance
     def animate(self):
         animation = self.animations[self.status]
         self.frame_index += self.animation_speed
@@ -86,8 +113,10 @@ class Enemy(Entity):
         self.image = animation[int(self.frame_index)]
         self.rect = self.image.get_rect(center = self.hitbox.center)
     def update(self):
+        self.hit_reaction()
         self.move(self.speed)
         self.animate()
+        self.check_death()
 
     def enemy_update(self,player):
         self.get_status(player)
